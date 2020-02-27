7b5a13cb38caa9e8129b4d7f5a65736edbe5b2ec
June <a1150927469@gmail.com>
2020-02-27 11:09:17 -0700

    Day1

--- Assignment3.py
+++ Assignment3.py
@@ -29,29 +29,85 @@ class Linked_List:
                 self.Node1=temp.next
                 temp=none
                 return
-        while(temp is not None):
+        while temp is not None:
             if temp.data==item:
-                prev=temp
-                temp=temp.next
+                break
+            prev=temp
+            temp=temp.nextNode
         if temp==None:
             return
-        prev.next = temp.next
+        prev.nextNode = temp.nextNode
         temp= None
+
+    def search(self,item):
+        temp = self.Node1
+        while temp is not None:
+            if temp.data==item:
+                return True
+            temp=temp.nextNode
+        return False
+
+    def isEmpty(self):
+        
+        if self.Node1 is not None:
+            return False
+        else:
+            return True
+
+    def size(self):
+        temp= self.Node1
+        count=0
+        while temp.data is not None:
+                count+=1
+                temp.data=temp.nextNode
+                if temp.nextNode is None:
+                    break
+                
+        print (count)
+        return
+                
+        
+
+            
 
         
         
     def printLL(self):
         printNode=self.Node1
         while printNode is not None:
-            print(printNode.data)
+            print (printNode.data)
             printNode=printNode.nextNode
 
 LL = Linked_List()
+if LL.isEmpty():
+    print("True")
+else:
+    print("False")
 LL.add(3)
 LL.add(4)
 LL.add(5)
 
 LL.printLL()
+if LL.search(4):
+    print("True")
+else:
+    print("False")
+if LL.isEmpty():
+    print("True")
+else:
+    print("False")
+LL.size()
+
+
 LL.remove(4)
 LL.printLL()
+if LL.search(4):
+    print("True")
+else:
+    print("False")
+
+
+
+
+
         
