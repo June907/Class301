23144568f66d410cd8b3303ea0354b211f67afc2
June <a1150927469@gmail.com>
2020-03-05 09:09:29 -0700

    Day7

--- Dequeue.py
+++ Dequeue.py
@@ -11,9 +11,9 @@ class Deque:
     def addFront(self,item):
           
         if self.head is None: 
-            self.head = Node(data) 
+            self.head = Node(item) 
         else: 
-            new_node = Node(data) 
+            new_node = Node(item) 
             self.head.prev = new_node 
             new_node.next = self.head 
             new_node.prev = None
@@ -37,5 +37,28 @@ class Deque:
         self.head=self.head.next
         
         return temp.data
+
+    def removeRear(self):
+        
+        while(self.head.next is not None):
+            self.head=self.head.next
+        temp=self.head
+        self.head=None
+        return temp.data
+
+    def print(self,node):
+        while(node is not None):
+            print(node.data, end=" ")
+            last = node
+            node= node.next
+
+de=Deque()
+de.addFront(5)
+de.addFront(7)
+de.addFront(9)
+de.print(de.head)
+
+de.removeRear()
+de.print(de.head)
         
         
--- tester.py
+++ tester.py
@@ -1,48 +1,19 @@
-# A linked list node 
-class Node: 
-  
-    # Constructor to create a new node 
-    def __init__(self, data): 
-        self.data = data 
-        self.next = None
-        self.prev = None
-  
-# Class to create a Doubly Linked List 
-class DoublyLinkedList: 
-  
-    # Constructor for empty Doubly Linked List 
-    def __init__(self): 
-        self.head = None
-  
-    # Given a reference to the head of a list and an 
-    # integer, inserts a new node on the front of list 
-    def push(self, new_data): 
-  
-        # 1. Allocates node 
-        # 2. Put the data in it 
-        new_node = Node(new_data) 
-  
-        # 3. Make next of new node as head and 
-        # previous as None (already None) 
-        new_node.next = self.head 
-  
-        # 4. change prev of head node to new_node 
-        if self.head is not None: 
-            self.head.prev = new_node 
-  
-        # 5. move the head to point to the new node 
-        self.head = new_node
-    def printList(self, node): 
-  
-        print ("\nTraversal in forward direction")
-        while(node is not None): 
-            print (" % d" %(node.data)), 
-            last = node 
-            node = node.nextNode
-  
-        print ("\nTraversal in reverse direction")
-        while(last is not None): 
-            print (" % d" %(last.data)), 
-            last = last.prev
-D=Node(8)
-print(D.prev)
+def collatz(n):
+    print(n, end=' ')
+    if n == 1:
+        return
+    elif n%2 == 0:
+        return collatz(n // 2)
+    else:
+        return collatz(3 * n + 1)
+
+def test_fun(x,y):
+    print(x,y)
+    if x == y:
+        return collatz(x)
+    elif x > y:
+        return test_fun(x - 1, y)
+    else:
+        return test_fun(x,y-1)
+
+test_fun(3,4)
